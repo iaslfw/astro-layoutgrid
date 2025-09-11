3eb5b2ca988aa7dabf6899634ab4c8ad3e26fa62
Sebastian Wolf <sebastian.wolf298@gmail.com>
2025-09-11 18:33:24 +0200

    continued on shipping v1

--- package/index.ts
+++ package/index.ts
@@ -2,5 +2,8 @@
 // Then, use this file to export everything you want your user to access.
 
 import Layoutgrid from './src/Layoutgrid.astro';
-
 export default Layoutgrid;
+
+// Export all types for TypeScript users (excluding the main props to avoid duplicates)
+export type { Breakpoint, GridData } from './src/types';
+export type { LayoutgridProps as GridProps } from './src/types';
