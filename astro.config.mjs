e8c872f5aff1c85a159e634ca0d0c6781bdbaef4
Sebastian Wolf <sebastian.wolf298@gmail.com>
2025-09-13 18:24:13 +0200

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    // @ts-expect-error
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [],
+  adapter: netlify(),
+  output: "server",
+});
